6213a74c0be83abb3b9f92ac831e36d482887d86
Sergio Ramos <sergirams@gmail.com>
2018-10-16 23:02:39 -0500

    cambios

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,7 +10,7 @@ class Cors{
         return $next($request)
             ->header('Access-Control-Allow-Origin','*')
             ->header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,OPTIONS')
-            ->header('Access-Control-Allow-Headers','Origin, Content-Type, X-Auth-Token','X-Auth-Token','X-CSRF-TOKEN');
+            ->header('Access-Control-Allow-Headers','Origin, Content-Type, X-Auth-Token','X-Auth-Token','X-CSRF-TOKEN','Authorization', 'X-Requested-With');
 
     }
 }
