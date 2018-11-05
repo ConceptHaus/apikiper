1eb5bd850a5080f00ba1317d3f46b12122c35664
Sergio Ramos <sergirams@gmail.com>
2018-11-05 13:11:38 -0600

    status con formato en perfil

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -33,8 +33,28 @@ class UserController extends Controller
                             ->join('status_oportunidad','status_oportunidad.id_oportunidad','colaborador_oportunidad.id_oportunidad')
                             ->join('cat_status_oportunidad','cat_status_oportunidad.id_cat_status_oportunidad','status_oportunidad.id_cat_status_oportunidad')
                             ->where('colaborador_oportunidad.id_colaborador',$id_user)
-                            ->select(DB::raw('count(*) as total, cat_status_oportunidad.status'))->groupBy('cat_status_oportunidad.status')
+                            ->where('cat_status_oportunidad.id_cat_status_oportunidad',1)
+                            ->select('cat_status_oportunidad.id_cat_status_oportunidad','cat_status_oportunidad.color',DB::raw('count(*) as total, cat_status_oportunidad.status'))->groupBy('cat_status_oportunidad.status')
                             ->get();
+
+        $status_2 = DB::table('oportunidades')
+                            ->join('colaborador_oportunidad','colaborador_oportunidad.id_oportunidad','oportunidades.id_oportunidad')
+                            ->join('status_oportunidad','status_oportunidad.id_oportunidad','colaborador_oportunidad.id_oportunidad')
+                            ->join('cat_status_oportunidad','cat_status_oportunidad.id_cat_status_oportunidad','status_oportunidad.id_cat_status_oportunidad')
+                            ->where('colaborador_oportunidad.id_colaborador',$id_user)
+                            ->where('cat_status_oportunidad.id_cat_status_oportunidad',2)
+                            ->select('cat_status_oportunidad.id_cat_status_oportunidad','cat_status_oportunidad.color',DB::raw('count(*) as total, cat_status_oportunidad.status'))->groupBy('cat_status_oportunidad.status')
+                            ->get();
+
+        $status_3 = DB::table('oportunidades')
+                            ->join('colaborador_oportunidad','colaborador_oportunidad.id_oportunidad','oportunidades.id_oportunidad')
+                            ->join('status_oportunidad','status_oportunidad.id_oportunidad','colaborador_oportunidad.id_oportunidad')
+                            ->join('cat_status_oportunidad','cat_status_oportunidad.id_cat_status_oportunidad','status_oportunidad.id_cat_status_oportunidad')
+                            ->where('colaborador_oportunidad.id_colaborador',$id_user)
+                            ->where('cat_status_oportunidad.id_cat_status_oportunidad',3)
+                            ->select('cat_status_oportunidad.id_cat_status_oportunidad','cat_status_oportunidad.color',DB::raw('count(*) as total, cat_status_oportunidad.status'))->groupBy('cat_status_oportunidad.status')
+                            ->get();
+
 
         $recordatorios = DB::table('recordatorios_prospecto')
                         ->join('detalle_recordatorio_prospecto','detalle_recordatorio_prospecto.id_recordatorio_prospecto','recordatorios_prospecto.id_recordatorio_prospecto')
@@ -45,9 +65,9 @@ class UserController extends Controller
             'user'=>$this->guard()->user(),
             'detalle'=>$detalle,
             'oportunidades'=>[
-                'status_1'=>'',
-                'status_2'=>'',
-                'status_3'=>''
+                'status_1'=>$status_1,
+                'status_2'=>$status_2,
+                'status_3'=>$status_3
             ],
             'recordatorios'=>$recordatorios
         ],200);
