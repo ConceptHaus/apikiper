a7cd5165c9e3568ad131be52488a9968aafe4633
Sergio Ramos <sergirams@gmail.com>
2018-11-09 11:49:34 -0600

    cambios google

--- app/Http/Controllers/Integraciones/GoogleController.php
+++ app/Http/Controllers/Integraciones/GoogleController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Google;
 use LaravelGmail;
+use Dacastro4\LaravelGmail\Services\Message\Mail;
 
 class GoogleController extends Controller
 {
@@ -20,10 +21,25 @@ public function googleApiCallback(){
 
     
     LaravelGmail::makeToken();
-    $id = '166f4226948f3c73';
-    $message = LaravelGmail::message()->get($id);
+    // $id = '166f4226948f3c73';
+    // $message = LaravelGmail::message()->get($id);
+    
+    $mail = new Mail;
+    $mail->to('sergirams@gmail.com')->from('sergio@concepthaus.mx')->subject('Prueba')->message('Prueba')->send();
+    
 
-    return response()->json($message->getPlainTextBody);
+    return response()->json($mail);
+
+        // foreach ( $messages as $message ) {
+        //     $mensaje = LaravelGmail::message()->get($message->id);
+
+        //     $body = $mensaje->getSubject();
+        //     $subject = $mensaje->getSubject();
+
+        //     return response()->json($body);
+        // }
+        //return $messages;
+    
 
 }
 
