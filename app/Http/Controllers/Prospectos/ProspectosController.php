7b57e7e0c6c33e3c27be2e8dc215bfdffb08962d
Yourname <yourmail@here.com>
2018-11-27 11:54:30 -0600

    url fuente no contactados

--- app/Http/Controllers/Prospectos/ProspectosController.php
+++ app/Http/Controllers/Prospectos/ProspectosController.php
@@ -248,8 +248,9 @@ class ProspectosController extends Controller
     public function getProspectosNoContactado (){
       $prospectos_sin_contactar = Prospecto::join('status_prospecto','prospectos.id_prospecto','status_prospecto.id_prospecto')
                                   ->join('detalle_prospecto','prospectos.id_prospecto','detalle_prospecto.id_prospecto')
+                                  ->join('cat_fuentes','prospectos.fuente','cat_fuentes.id_fuente')
                                   ->where('status_prospecto.id_cat_status_prospecto','=',2)
-                                  ->select('prospectos.id_prospecto','prospectos.nombre','prospectos.apellido','prospectos.correo','prospectos.fuente','prospectos.deleted_at','prospectos.created_at','prospectos.updated_at','status_prospecto.id_status_prospecto','status_prospecto.id_cat_status_prospecto','detalle_prospecto.whatsapp')
+                                  ->select('prospectos.id_prospecto','prospectos.nombre','prospectos.apellido','prospectos.correo','cat_fuentes.nombre as fuente','cat_fuentes.id_fuente as id_fuente','cat_fuentes.url as url_fuente','prospectos.deleted_at','prospectos.created_at','prospectos.updated_at','status_prospecto.id_status_prospecto','status_prospecto.id_cat_status_prospecto','detalle_prospecto.whatsapp')
                                   ->orderBy('status_prospecto.updated_at', 'desc')
                                   ->get();
 
@@ -451,10 +452,17 @@ class ProspectosController extends Controller
 
     public function getEtiquetas($id){
         $prospecto_etiquetas = Prospecto::GetProspectoEtiquetas($id);
+
+        if ($prospecto_etiquetas) {
+          return response()->json([
+              'message'=>'Etiquetas obtenidas correctamente.',
+              'error'=>false,
+              'data'=>$prospecto_etiquetas
+          ],200);
+        }
         return response()->json([
-            'message'=>'Correcto',
-            'error'=>false,
-            'data'=>$prospecto_etiquetas
+            'message'=>'El prospecto no tiene etiquetas.',
+            'error'=>false
         ],200);
     }
 
