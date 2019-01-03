66b88e92e41c69cdf6db7272ec8255ca506d12b7
fcastaneda9725 <f.castanedatrujillo@gmail.com>
2019-01-03 12:36:35 -0600

    post para crear mailings v1

--- app/Http/Controllers/Mailing/MailingController.php
+++ app/Http/Controllers/Mailing/MailingController.php
@@ -9,11 +9,48 @@ use Illuminate\Http\Response;
 use App\Modelos\Mailing\Mailings;
 use App\Modelos\Mailing\DetalleMailings;
 use App\Modelos\Mailing\ImagesMailings;
+use Illuminate\Support\Facades\Validator;
 use Mailgun;
+use DB;
 
 class MailingController extends Controller
 {
+    // protected function validadorCamapaña(array $data){
+    //   return Validator::make($data, [
+    //     ''
+    //   ]);
+    // }
+
     public function addNew(Request $request){
+
+      $campaña = $request->all();
+
+      try {
+        DB::beginTransaction();
+        $campana = new Mailings;
+        $campana->titulo_campana = $request->nombre_campana;
+        $campana->save();
+
+        $mailing = new DetalleMailings;
+        $mailing->subject = $request->titulo;
+        $mailing->preview_text = $request->subtitulo;
+        $mailing->text_body = $request->descripcion;
+        $mailing->cta_nombre = $request->nombre_boton;
+        $mailing->cta_url = $request->url;
+        $mailing->color = $request->color;
+        $campana->detalle()->save($mailing);
+        DB::commit();
+
+        return response()->json([
+          'message'=>'Camapaña guardada correctamente.',
+          'error'=>false
+        ],200);
+      } catch (Exception $e) {
+        return response()->json([
+          'message'=>$e,
+          'error'=>true
+        ],400);
+      }
 
     }
 
