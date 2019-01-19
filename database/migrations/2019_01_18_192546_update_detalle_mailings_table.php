0ea91eebf29e3ae2eaeb7a5287c5c064c9c658a4
OmarAbaroa <alvaro1295@hotmail.com>
2019-01-18 19:43:48 -0600

    Arregando error de laves en detalle mailing

--- database/migrations/2019_01_18_192546_update_detalle_mailings_table.php
+++ database/migrations/2019_01_18_192546_update_detalle_mailings_table.php
@@ -14,14 +14,16 @@ class UpdateDetalleMailingsTable extends Migration
     public function up()
     {
         Schema::table('detalle_mailings', function (Blueprint $table){
-            
-            //$table->integer('opcion_servicio')->unsigned()->default(1);
+            $table->dropColumn('opcion_servicio');
+            $table->dropColumn('opcion_etiqueta');
+            $table->dropColumn('opcion_status');
+            $table->integer('opcion_servicio')->unsigned()->default(1);
             $table->foreign('opcion_servicio')->references('id_servicio_cat')->on('cat_servicios')->onDelete('cascade');
             //ligado con la tabla de cat_servicios
-            //$table->integer('opcion_etiqueta')->unsigned()->default(1);
+            $table->integer('opcion_etiqueta')->unsigned()->default(1);
             $table->foreign('opcion_etiqueta')->references('id_etiqueta')->on('etiquetas')->onDelete('cascade');
             //ligado con la tabla de etiquetas
-            //$table->integer('opcion_status')->unsigned()->default(1);
+            $table->integer('opcion_status')->unsigned()->default(1);
             $table->foreign('opcion_status')->references('id_cat_status_oportunidad')->on('cat_status_oportunidad')->onDelete('cascade');
             //ligado con la tabla de cat_status_oportunidad
             //$table->string('fondo_general');
