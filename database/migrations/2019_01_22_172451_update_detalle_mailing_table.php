bd040837c388fd3a745a1fd2f66788d360c0ef69
OmarAbaroa <alvaro1295@hotmail.com>
2019-01-30 13:32:39 -0600

    Modificaondo migración de mailling

--- database/migrations/2019_01_22_172451_update_detalle_mailing_table.php
+++ database/migrations/2019_01_22_172451_update_detalle_mailing_table.php
@@ -21,11 +21,11 @@ class UpdateDetalleMailingTable extends Migration
             $table->string('color')->nullable()->default(null)->change();
             $table->string('color_fuente')->nullable()->default(null)->change();
             $table->string('color_cta')->nullable()->default(null)->change();
-            $table->string('fondo_general')->nullable()->default(null)->change();
-            $table->string('fondo_cta')->nullable()->default(null)->change();
-            $table->string('color_titulo')->nullable()->default(null)->change();
-            $table->string('color_subtitulo')->nullable()->default(null)->change();
-            $table->string('color_lineas')->nullable()->default(null)->change();
+            $table->string('fondo_general')->nullable()->default(null);
+            $table->string('fondo_cta')->nullable()->default(null);
+            $table->string('color_titulo')->nullable()->default(null);
+            $table->string('color_subtitulo')->nullable()->default(null);
+            $table->string('color_lineas')->nullable()->default(null);
 
 
         });
