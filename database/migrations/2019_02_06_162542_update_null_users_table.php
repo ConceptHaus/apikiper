cea8aaf704aff0cf055fe5f0513d920381cb4468
Sergio Ramos <sergirams@gmail.com>
2019-02-06 16:29:02 -0600

    migracion para hacer apellido null

---
+++ database/migrations/2019_02_06_162542_update_null_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateNullUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('prospectos', function (Blueprint $table) {
+            $table->string('apellido')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+    }
+}
