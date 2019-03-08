aa742c6b30177c86ab45c83950cd0b2764d8aea6
OmarAbaroa <alvaro1295@hotmail.com>
2019-03-08 15:21:36 -0600

    Arreglando issue Kiper#268 verificando si existe o no super admin

--- app/Listeners/HistorialListener.php
+++ app/Listeners/HistorialListener.php
@@ -31,19 +31,21 @@ class HistorialListener
     {
         $actividad = $event->evento;
         $super_admin = User::where('super_admin', '=', 1)->first();
-        $data['nombre_causante'] = $actividad->causer->nombre. ' ' . $actividad->causer->apellido;
-        $data['correo_causante'] = $actividad->causer->email;
-        $data['actividad'] = $actividad->properties['accion'] . ' ' . $actividad->log_name;
-        $data['fecha'] = $actividad->created_at;
-        $data['email_para'] = $super_admin->email;
-        $data['nombre_para'] = $super_admin->nombre . ' ' . $super_admin->apellido;
-        $data['asunto'] = 'Actualización en Kiper!';
-        $data['email_de'] = 'activity@kiper.app';
-        $data['nombre_de'] = 'Actividad de Kiper';
-        Mailgun::send('mailing.mailSuperAdmin', $data, function ($message) use ($data){
-            $message->from($data['email_de'],$data['nombre_de']);
-            $message->subject($data['asunto']);
-            $message->to($data['email_para'],$data['nombre_para']);  
-        });
+        if( $super_admin ) {
+            $data['nombre_causante'] = $actividad->causer->nombre. ' ' . $actividad->causer->apellido;
+            $data['correo_causante'] = $actividad->causer->email;
+            $data['actividad'] = $actividad->properties['accion'] . ' ' . $actividad->log_name;
+            $data['fecha'] = $actividad->created_at;
+            $data['email_para'] = $super_admin->email;
+            $data['nombre_para'] = $super_admin->nombre . ' ' . $super_admin->apellido;
+            $data['asunto'] = 'Actualización en Kiper!';
+            $data['email_de'] = 'activity@kiper.app';
+            $data['nombre_de'] = 'Actividad de Kiper';
+            Mailgun::send('mailing.mailSuperAdmin', $data, function ($message) use ($data){
+                $message->from($data['email_de'],$data['nombre_de']);
+                $message->subject($data['asunto']);
+                $message->to($data['email_para'],$data['nombre_para']);  
+            });
+        }
     }
 }
