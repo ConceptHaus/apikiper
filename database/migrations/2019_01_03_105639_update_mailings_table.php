cb62114100a6be8af3006a6c192ab70d72d18812
Sergio Ramos <sergirams@gmail.com>
2019-03-13 19:56:48 -0600

    error en migraciones

--- database/migrations/2019_01_03_105639_update_mailings_table.php
+++ database/migrations/2019_01_03_105639_update_mailings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateMailingsTable extends Migration
+class UpdatesMailingsTable extends Migration
 {
     /**
      * Run the migrations.
