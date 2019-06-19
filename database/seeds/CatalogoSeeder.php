d7665dcf19199064c2cb25df7858cbd3caebbc34
Sergio Ramos <sergirams@gmail.com>
2019-06-19 13:38:15 -0500

    Seeder categorias

---
+++ database/seeds/CatalogoSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CatalogoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Categorias fuentes
+        DB::table('cat_fuentes')->insert(['nombre'=>'Facebook','url'=>'https://s3.us-east-2.amazonaws.com/kiperbucket/generales/facebook.svg','status'=>1 ,'created_at'=> now()]);
+        DB::table('cat_fuentes')->insert(['nombre'=>'Google','url'=>'https://s3.us-east-2.amazonaws.com/kiperbucket/generales/google.svg','status'=>1 ,'created_at'=> now()]);
+        DB::table('cat_fuentes')->insert(['nombre'=>'Manual','url'=>'https://s3.us-east-2.amazonaws.com/kiperbucket/generales/manual.svg','status'=>1 ,'created_at'=> now()]);
+        DB::table('cat_fuentes')->insert(['nombre'=>'Llamada','url'=>'https://s3.us-east-2.amazonaws.com/kiperbucket/generales/incoming-call2.svg','status'=>1 ,'created_at'=> now()]);
+        //Categorias Status Oportunidad
+        DB::table('cat_status_oportunidad')->insert(['status'=>'Cotizado','color'=>'#fac219','created_at'=> now()]);
+        DB::table('cat_status_oportunidad')->insert(['status'=>'Cerrado','color'=>'#37af9b','created_at'=> now()]);
+        DB::table('cat_status_oportunidad')->insert(['status'=>'No viable','color'=>'#000000','created_at'=> now()]);
+        //Categorias Status Prospecto
+        DB::table('cat_status_prospecto')->insert(['status'=>'Contactado','color'=>'#000000','created_at'=> now()]);
+        DB::table('cat_status_prospecto')->insert(['status'=>'No contactado','color'=>'#000000','created_at'=> now()]);
+        //Categoria Medio Contacto
+        DB::table('mediocontacto_catalogo')->insert(['nombre'=>'Nota','color'=>'#ffd505','created_at'=> now()]);
+        DB::table('mediocontacto_catalogo')->insert(['nombre'=>'Llamada','color'=>'#297c35','created_at'=> now()]);
+        DB::table('mediocontacto_catalogo')->insert(['nombre'=>'Whatsapp','color'=>'#33CC66','created_at'=> now()]);
+        DB::table('mediocontacto_catalogo')->insert(['nombre'=>'Correo','color'=>'#408af9','created_at'=> now()]);
+        DB::table('mediocontacto_catalogo')->insert(['nombre'=>'Reunión','color'=>'#f9ab3f','created_at'=> now()]);
+
+    }
+}
