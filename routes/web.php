435f4053c9b62362b79b609184ae27d54f820fa3
Sergio Ramos <sergirams@gmail.com>
2019-07-11 16:35:42 -0500

    ruta

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function(){
+    return response()->json(['Error'=>'Nothing here. ⚠️'],200);
+});
