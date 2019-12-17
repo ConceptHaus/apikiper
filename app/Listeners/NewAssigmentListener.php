4aa7565ab51b72e22abe1d16b6e200e2608a52e5
Sergio Ramos <sergirams@gmail.com>
2019-12-16 18:43:18 -0600

    envio de mails a admins

--- app/Listeners/NewAssigmentListener.php
+++ app/Listeners/NewAssigmentListener.php
@@ -201,11 +201,16 @@ class NewAssigmentListener
         $date = Carbon::today();
         
         if($event->evento['desarrollo'] === 'polanco'){
+            $adminstradores = User::where('rol',1)->get();
+            foreach($adminstradores as $admin){
+                $this->sendMail($admin->id,$event->evento['prospecto']);
+            }
             foreach($assigment_gfa['ejecutivo_1']['fechas'] as $dia){
             
                 if($date->equalTo($dia)){
     
                     $this->assign($assigment_gfa['ejecutivo_1']['id'], $event->evento['prospecto']);
+                    
                 }
             }
     
@@ -217,11 +222,16 @@ class NewAssigmentListener
                 }
             }
         }else{
+            $adminstradores = User::where('rol',2)->get();
+            foreach($adminstradores as $admin){
+                $this->sendMail($admin->id,$event->evento['prospecto']);
+            }
             foreach($assigment_gfa['ejecutivo_3']['fechas'] as $dia){
             
                 if($date->equalTo($dia)){
     
                     $this->assign($assigment_gfa['ejecutivo_3']['id'], $event->evento['prospecto']);
+
                 }
             }
     
@@ -246,36 +256,46 @@ class NewAssigmentListener
             $pivot_col_pros->id_prospecto = $prospecto->id_prospecto;
             $pivot_col_pros->save();
 
-            $data['email'] = $colaborador->email;
+            $this->sendMail($id, $prospecto);
+            $this->sendSMS($id, $prospecto);
+
+    }
+
+    public function sendMail($id, $prospecto){
+        $colaborador = User::where('id',$id)->first();
+
+        $data['email'] = $colaborador->email;
            
-            $data['asunto'] = 'Tienes un nuevo prospecto 😁 🎉';
-            $data['email_de'] = 'activity@kiper.io';
-            $data['nombre_de'] = 'Kiper';
+        $data['asunto'] = 'Tienes un nuevo prospecto 😁 🎉';
+        $data['email_de'] = 'activity@kiper.io';
+        $data['nombre_de'] = 'Kiper';
 
-            $data['nombre_p'] = $prospecto->nombre;
-            $data['apellido_p'] = $prospecto->apellido;
-            $data['correo_p'] = $prospecto->correo;
-            $data['empresa_p'] = $prospecto->detalle_prospecto->empresa ?? 'not set';
-            $data['telefono_p'] = $prospecto->detalle_prospecto->telefono;
-            $data['mensaje_p'] = $prospecto->detalle_prospecto->nota ?? 'not set';
-            $data['campaign_p'] = (isset($prospecto->campaign->utm_campaign) ? $prospecto->campaign->utm_campaign : 'orgánico');
-            $data['term_p'] = (isset($prospecto->campaign->utm_term) ? $prospecto->campaign->utm_term : 'orgánico');
+        $data['nombre_p'] = $prospecto->nombre;
+        $data['apellido_p'] = $prospecto->apellido;
+        $data['correo_p'] = $prospecto->correo;
+        $data['empresa_p'] = $prospecto->detalle_prospecto->empresa ?? 'not set';
+        $data['telefono_p'] = $prospecto->detalle_prospecto->telefono;
+        $data['mensaje_p'] = $prospecto->detalle_prospecto->nota ?? 'not set';
+        $data['campaign_p'] = (isset($prospecto->campaign->utm_campaign) ? $prospecto->campaign->utm_campaign : 'orgánico');
+        $data['term_p'] = (isset($prospecto->campaign->utm_term) ? $prospecto->campaign->utm_term : 'orgánico');
 
-            
-            Mailgun::send('mailing.template_newlead',$data, function($message) use ($data){
-                $message->from($data['email_de'],$data['nombre_de']);
-                $message->subject($data['asunto']);
-                $message->to($data['email']);
-                $message->trackOpens(true);
-                $message->tag('new_lead');
-            });
-            $colaborador = User::find($id);
-            if(isset($colaborador->detalle) && count($colaborador->detalle->celular)==10){
+        Mailgun::send('mailing.template_newlead',$data, function($message) use ($data){
+            $message->from($data['email_de'],$data['nombre_de']);
+            $message->subject($data['asunto']);
+            $message->to($data['email']);
+            $message->trackOpens(true);
+            $message->tag('new_lead');
+        });
+    }
+
+    public function sendSMS($id,$prospecto){
+        $colaborador = User::find($id);
+            if(isset($colaborador->detalle) && $colaborador->detalle->celular){
                 $this->twilioClient->messages->create(
                 '+52'.$colaborador->detalle->celular,
                 array(
                     "from" => $this->sendingNumber,
-                    "body" => 'Kiper Leads | Nombre: '.$data['nombre_p'].' '.$data['apellido_p'].' Correo: '.$data['correo_p'].' Telefono: '.$data['telefono_p']
+                    "body" => 'Kiper Leads | Nombre: '.$prospecto->nombre.' '.$prospecto->apellido.' Correo: '.$prospecto->correo.' Telefono: '.$prospecto->detalle_prospecto->telefono
                 ));
             }
     }
