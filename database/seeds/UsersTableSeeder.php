cd8cd82feb01481eed4e19dacaa670363370336a
interaxdev <33502820+interaxdev@users.noreply.github.com>
2021-08-17 15:38:37 -0500

    Modiifcacion para insertar una etiqueta solamente y modificacion al Seeder principal para insertar los registros necesarios para una base de datos vacia - CA

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Modelos\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $cat_status_oportunidad             = new User();
+        $cat_status_oportunidad->nombre     = "Usuario Test";
+        $cat_status_oportunidad->apellido   = "Admin Kiper ";
+        $cat_status_oportunidad->email      = "adminuser@concepthaus.mx";
+        $cat_status_oportunidad->password   = '$2y$10$xVH1mAebx68O.4GqUbTVleNQ1DA2qiHjx7oZ8xphF0.Gkg.8OaNMC'; //prueba123
+        $cat_status_oportunidad->status     = 1;
+        $cat_status_oportunidad->role_id    = 5;
+        $cat_status_oportunidad->save();
+    }
+}
