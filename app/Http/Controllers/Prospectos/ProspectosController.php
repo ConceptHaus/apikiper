f42bdbe90e577fdb331006df6ca8c2269f620569
kiperSusana <susana@kiper.app>
2022-04-18 20:18:42 -0500

    se quitan agregados de pruebas 18-04-22

--- app/Http/Controllers/Prospectos/ProspectosController.php
+++ app/Http/Controllers/Prospectos/ProspectosController.php
@@ -1085,7 +1085,7 @@ class ProspectosController extends Controller
         ]);
     }
 
-    public function downloadProspectos($role_id, $rol, $id_user, $correos, $nombre, $telefono, $status, $grupo, $etiquetas, $fechaInicio, $fechaFin, $colaboradores, $busqueda, $idIntSoc){
+    public function downloadProspectos($role_id, $rol, $id_user, $correos, $nombre, $telefono, $status, $grupo, $etiquetas, $fechaInicio, $fechaFin, $colaboradores, $busqueda){
         $correos = json_decode($correos);
         $nombre = json_decode($nombre);
         $telefono = json_decode($telefono);
@@ -1096,8 +1096,6 @@ class ProspectosController extends Controller
         $fechaFin = json_decode($fechaFin);
         $colaboradores = json_decode($colaboradores);
         $busqueda = json_decode($busqueda);
-        //campos nuevos 
-        $idIntSoc = json_decode($idIntSoc);
 
         $usuario = $this->userServ->findById($id_user);
         $roles = $this->roleServ->findById($usuario->role_id);
@@ -1117,8 +1115,7 @@ class ProspectosController extends Controller
             'Comentarios',
             'Seguimiento',
             'Etiquetas',
-            'Empresa',
-            'id_interno'
+            'Empresa'
         ];
 
         if($rol == OldRole::POLANCO){
@@ -1138,7 +1135,7 @@ class ProspectosController extends Controller
                 'message'=>'No tienes permiso',
                 'error'=>true],401);
         }
-        return (new ProspectosReports($headings,$desarrollo,$id_user, $correos, $nombre, $telefono, $status, $fuente, $etiqueta, $fechaInicio, $fechaFin, $colaboradores, $busqueda, $idIntSoc))->download("{$date}_{$desarrollo}_reporte.xlsx");
+        return (new ProspectosReports($headings,$desarrollo,$id_user, $correos, $nombre, $telefono, $status, $fuente, $etiqueta, $fechaInicio, $fechaFin, $colaboradores, $busqueda))->download("{$date}_{$desarrollo}_reporte.xlsx");
     }
 
     //Functiones auxiliares
