34c1b0e27e783b366d0754e9d386f5f2a1c764f4
kiperSusana <susana@kiper.app>
2022-04-29 12:02:51 -0500

    modelo de la nueva tabla

---
+++ app/Modelos/Oportunidad/ObjecionesOportunidad.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Modelos\Oportunidad;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class EtiquetasOportunidad extends Model
+{
+    use \Askedio\SoftCascade\Traits\SoftCascadeTrait;
+    use SoftDeletes;
+
+    protected $table = 'etiquetas_oportunidades';
+    protected $primaryKey = 'id_etiqueta_oportunidad';
+    protected $fillable = [
+        'id_objecion_oportunidad',
+        'id_objecion',
+        'id_oportunidad'
+        
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function oportunidad(){
+        return $this->belongsTo('App\Modelos\Oportunidad\Oportunidad','id_oportunidad','id_oportunidad');
+    }
+
+    // public function etiqueta(){
+    //     return $this->belongsTo('App\Modelos\Extras\Etiqueta','id_etiqueta','id_etiqueta');
+    // }
+
+}
