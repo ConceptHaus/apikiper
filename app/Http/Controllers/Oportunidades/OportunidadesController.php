9de2809604b150706bbd4f50637c6d333f8f8ce4
kiperSusana <susana@kiper.app>
2022-05-03 21:04:00 -0500

    se modifica oportunidad controller 12

--- app/Http/Controllers/Oportunidades/OportunidadesController.php
+++ app/Http/Controllers/Oportunidades/OportunidadesController.php
@@ -725,7 +725,7 @@ class OportunidadesController extends Controller
             $valor = str_replace(',', '', $valor);
             $meses = intval($request->meses);
             // $confirmacion_cotizacion = str_replace($request->$confirmacion_cotizacion);
-            $confirmacion_cotizacion = str_replace($request->$confirmacion_cotizacion);
+            // $confirmacion_cotizacion = str_replace($request->$confirmacion_cotizacion);
             $detalle->valor = $valor;
             $detalle->meses = $meses;
             $detalle->confirmacion_cotizacion = $confirmacion_cotizacion;
