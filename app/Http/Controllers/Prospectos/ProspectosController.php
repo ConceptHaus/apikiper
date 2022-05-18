1213f731e3703a5aca3ce524da45a246397f014e
kiperSusana <susana@kiper.app>
2022-05-17 22:39:39 -0500

    prospecto controllador para nueva oportunidad 7

--- app/Http/Controllers/Prospectos/ProspectosController.php
+++ app/Http/Controllers/Prospectos/ProspectosController.php
@@ -184,8 +184,8 @@ class ProspectosController extends Controller
                             $valor = str_replace('$ ', '',$oportunidad['valor']);
                             $valor = str_replace(',', '', $valor);
                             $detalle_oportunidad->valor = $valor;
-                            $detalle_oportunidad -> checkin_opor = $request -> checkin_opor;
-                            $detalle_oportunidad -> checkout_opor = $request -> checkout_opor;
+                            $detalle_oportunidad -> checkin_opor = $request -> fecha_ingreso;
+                            $detalle_oportunidad -> checkout_opor = $request -> fecha_salida;
                         }
                         else{
                             $detalle_oportunidad->valor = 0;
