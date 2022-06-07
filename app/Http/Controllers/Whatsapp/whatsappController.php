897e7c08b7582350fac7a63ea5be312dd73528f9
kiperSusana <susana@kiper.app>
2022-06-07 08:07:24 -0500

    prueba 1

--- app/Http/Controllers/Whatsapp/whatsappController.php
+++ app/Http/Controllers/Whatsapp/whatsappController.php
@@ -31,19 +31,6 @@ use App\Modelos\Empresa\Empresa;
 use App\Modelos\Empresa\EmpresaProspecto;
 use App\Http\Services\Auth\AuthService;
 
-use App\Evento;
-use App\Modelos\Extras\RecordatorioProspecto;
-use App\Modelos\Extras\DetalleRecordatorioProspecto;
-use App\Modelos\Extras\DetalleEvento;
-use App\Modelos\Oportunidad\StatusOportunidad;
-use App\Modelos\Prospecto\StatusProspecto;
-use App\Modelos\Prospecto\CatStatusProspecto;
-use App\Events\Historial;
-use App\Events\Event;
-use \App\Http\Enums\Permissions;
-use App\Imports\ProspectosImport;
-use App\Exports\ProspectosReports;
-use Excel;
 
 use App\Http\Enums\OldRole;
 use App\Http\Services\Users\UserService;
@@ -64,45 +51,31 @@ class ProspectosController extends Controller
     }
 
     public function getChats($method, $url, $data){
-            $curl = curl_init();
-            switch ($method){
-               case "POST":
-                  curl_setopt($curl, CURLOPT_POST, 1);
-                  if ($data)
-                     curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                  break;
-            //    case "PUT":
-            //       curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-            //       if ($data)
-            //          curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
-            //       break;
-               default:
-                  if ($data)
-                     $url = sprintf("%s?%s", $url, http_build_query($data));
-            }
+        $curl = curl_init();
 
-            curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.ultramsg.com/instance4088/messages?token=yv47t9lpd8ruvou6&page=1&limit=30&status=all&sort=desc",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                "content-type: application/x-www-form-urlencoded"
-            ),
-            ));
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "https://api.ultramsg.com/instance4088/messages?token=yv47t9lpd8ruvou6&page=1&limit=30&status=all&sort=desc",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 30,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "GET",
+          CURLOPT_HTTPHEADER => array(
+            "content-type: application/x-www-form-urlencoded"
+          ),
+        ));
+        
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        
+        curl_close($curl);
+        
+        if ($err) {
+          echo "cURL Error #:" . $err;
+        } else {
+          echo $response;
+        }
 
-            $response = curl_exec($curl);
-            $err = curl_error($curl);
-
-            curl_close($curl);
-
-            if ($err) {
-            echo "cURL Error #:" . $err;
-            } else {
-            echo $response;
-            }
-         }
+    }
 }
