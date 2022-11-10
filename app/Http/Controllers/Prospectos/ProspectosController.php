4c24bb1f3a3d8cbbbc18f9b4dc13bd44a3e07d9a
sesai <sesai.cornejo@gmail.com>
2022-11-10 07:51:47 -0600

    nuevos valores de porcentaje y costo_final

--- app/Http/Controllers/Prospectos/ProspectosController.php
+++ app/Http/Controllers/Prospectos/ProspectosController.php
@@ -616,6 +616,8 @@ class ProspectosController extends Controller
                 $valor = str_replace(',', '', $valor);
                 $detalle_oportunidad->valor = $valor;
                 $detalle_oportunidad->meses = $request->meses;
+                $detalle_oportunidad->porcentaje = $request->porcentaje;
+                $detalle_oportunidad->valor_final = $request->valor_final;
                 $detalle_oportunidad->save();
 
                 //Cambio de Status Prospecto
--- app/Modelos/Oportunidad/DetalleOportunidad.php
+++ app/Modelos/Oportunidad/DetalleOportunidad.php
@@ -17,7 +17,9 @@ class DetalleOportunidad extends Model
         'id_oportunidad',
         'descripcion',
         'valor',
-        'meses'
+        'meses',
+        'porcentaje',
+        'valor_final'
     ];
 
     public function oportunidad(){
