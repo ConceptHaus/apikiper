bbd4c5e9f6b51bbf81f841d52786cc0ba1fdae92
Leslye Pérez <leslye@concepthaus.mx>
2022-11-28 23:43:34 -0600

    se agrega

--- app/Http/Controllers/Prospectos/ProspectosController.php
+++ app/Http/Controllers/Prospectos/ProspectosController.php
@@ -146,6 +146,7 @@ class ProspectosController extends Controller
                 }else {
                     $prospectoDetalle->empresa = $request->empresa;
                     $empresa = Empresa::where('nombre', '=', $request->empresa)->wherenull('deleted_at')->first();
+                    print_r($empresa); exit;
                         if($empresa){
                             $prospecto_empresa = new EmpresaProspecto;
                             $prospecto_empresa->id_empresa = $empresa->id_empresa;
